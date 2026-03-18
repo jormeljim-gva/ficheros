53134c0871a7d81574d48a6c05aefdaae74c4caa
Jordi Melchor Jimenez <jormeljim@alu.edu.gva.es>
2026-03-18 12:31:20 +0100

    Ejerciocios de ficheros incompletos

---
+++ src/main/java/Ejercicio5_2_2.java
@@ -0,0 +1,16 @@
+import com.google.gson.Gson;
+
+public class Ejercicio5_2_2 {
+    private int num;
+    private String nombre;
+    private int departamento;
+    private int edad;
+    private double sueldo;
+
+    // Método que se llama en el ejemplo de la imagen
+    public void display() {
+        System.out.println("Num: " + num + ", Nombre: " + nombre +
+                ", Departamento: " + departamento +
+                ", edad: " + edad + ", sueldo:" + sueldo);
+    }
+}
--- src/main/java/ejemplos/Ejercicio.java
+++ src/main/java/ejemplos/Ejercicio.java
@@ -12,13 +12,20 @@ public class Ejercicio{
         int cont = 0;
         int indice = 0;
         Scanner sc = new Scanner(System.in);
+
         while(indice!=-1){
+            cont = 1;
             System.out.println("Directorios y ficheros de " + f + "Pulse -1 para salir");
+            System.out.println("0 - Directorio padre");
             for (File fichero : f.listFiles()){
-                System.out.println(cont++ + " " + fichero.getName());
+                System.out.println(cont++ + " - " + fichero.getName());
             }
             indice = sc.nextInt();
             //Aquí hay que coger el File de listFiles con dicho indice
+            if (indice == 0)
+                f = f.getParentFile();
+            else
+                f = f.listFiles()[indice-1];
         }
     }
 }
